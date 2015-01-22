c1925bebf8a9ff40219f62fcfb852d927fd5bd43
CodersPress <support@coderspress.com>
2015-01-22 10:33:42 +0000

    Update convert.php

--- convert.php
+++ convert.php
@@ -1,21 +1,10 @@
 <?php
-
-$amount = $_POST['amount'];
-$from = $_POST['from'];
-$to = $_POST['to'];
-
-if($from == $to){
-    echo $amount;
-    exit;
-}
- 
-$string = $amount.$from.'%3D%3F'.$to;
-$url = 'http://www.google.com/ig/calculator?hl=en&q='. $amount . $from .'%3D%3F'. $to;
-$result = file_get_contents($url);
- 
-$result = explode('"', $result);
-$converted_amount = explode(' ', $result[3]);
-$conversion = $converted_amount[0];
-echo $conversion;
-
+$amount = urlencode($_POST['amount']);
+  $from = urlencode($_POST['from']);
+  $to = urlencode($_POST['to']);
+  $get = file_get_contents("https://www.google.com/finance/converter?a=$amount&from=$from&to=$to");
+  $get = explode("<span class=bld>",$get);
+  $get = explode("</span>",$get[1]);  
+  $converted_amount = preg_replace("/[^0-9\.]/", null, $get[0]);
+echo $converted_amount;
 ?>
